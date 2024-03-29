31eada74cd4b106bcd47b459e9793aed305b301c
DMendoza013 <damien.mendoza013@gmail.com>
2024-03-28 18:35:52 -0700

    added to form.css

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,3 +1,38 @@
-const userNameInput = document.querySelector('#username');
-const userTitle = document.querySelector('#title');
-const userContent = document.querySelector('#main-content')
+const userNameInput = document.getElementById('username');
+const userTitle = document.getElementById('title');
+const userContent = document.getElementById('main-content');
+const submitButton = document.getElementById('button');
+
+function saveBlog() {
+
+    const blog = {
+        userNameInput: userNameInput.value,
+        userTitle: userTitle.value,
+        userContent: userContent.value
+    }
+
+    localStorage.setItem('blog', JSON.stringify(blog));
+};
+
+
+function outputBlog () {
+
+    const lastBlog = JSON.parse(localStorage.getItem('blog'));
+
+    if (lastBlog !== null) {
+        document.getElementById('saved-userName').innerHTML = lastBlog.userNameInput;
+        document.getElementById('saved-title').innerHTML = lastBlog.userTitle;
+        document.getElementById('saved-content').innerHTML = lastBlog.userContent;
+    }
+};
+
+submitButton.addEventListener('click', function(event){
+    saveBlog();
+    outputBlog();
+    event.preventDefault();
+});
+
+ function init(){
+     outputBlog();
+ };
+init();
